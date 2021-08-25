ff3f316cbc53ebcb257deefc37f8e23c59cc10ba
mvilella <mvilella@obsa.ad>
2021-08-25 12:09:49 +0200

    Add main Pavilion section

---
+++ src/layout/pavilion/routes.js
@@ -0,0 +1,11 @@
+import Pavilion from './Pavilion.vue';
+
+export default [
+  {
+    path: '/pavilion',
+    components: {
+      default: Pavilion,
+    },
+    alias: ['/', '/pavello'],
+  },
+];
