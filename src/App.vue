6392aec8b94e273e5439c7c014cbddd317e39e12
mvilella <mvilella@obsa.ad>
2021-08-25 12:03:05 +0200

    Add router

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,50 @@
         <img id="main-logo" v-svg-inline src="/images/brand.svg">
         <nav-locales />
       </header>
+      <div class="at-bottom">
+        <router-view />
+      </div>
+      <router-link :to="`/${nextSection}`" class="nav-next">
+        {{ t(`${nextSection}.tagline`) }}<span class="arrow" />
+      </router-link>
     </main>
     <section id="column">
+      <router-view name="column" />
     </section>
     <section id="top">
+      <router-view name="top" />
     </section>
     <section id="bottom">
+      <router-view name="bottom" />
     </section>
   </div>
 </template>
 
 <script>
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { useRoute } from 'vue-router';
 import NavLocales from '/@/layout/NavLocales.vue';
+import config from '/@/config.yaml';
+
 export default {
   name: 'App',
   components: { NavLocales },
   setup() {
-    return {};
+    const { t, locale } = useI18n();
+
+    const nextSection = computed(() => {
+      const sections = Object.keys(config.sections);
+      // Current section
+      const { matched: [route] = [] } = useRoute();
+      const path = route?.aliasOf?.path || route?.path;
+      const index = Math.max(0, sections.indexOf(path?.substring(1)));
+      // Next section
+      const next = (index + 1) % sections.length;
+      return sections[next];
+    });
+
+    return { t, locale, nextSection };
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import SvgInline from 'vue-svg-inline-plugin';
 import i18n from './i18n';
+import router from './router';
 import App from './App.vue';
 
 import 'reset-css';
@@ -8,5 +9,6 @@ import './styles/main.scss';
 
 const app = createApp(App);
 app.use(i18n);
+app.use(router);
 app.use(SvgInline);
 app.mount('#app');
