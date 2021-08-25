569be7ae20fd7b099171d4e2fffb894a51b11bb7
mvilella <mvilella@obsa.ad>
2021-08-25 12:12:00 +0200

    Add Pavilion gallery carousel

--- src/layout/pavilion/routes.js
+++ src/layout/pavilion/routes.js
@@ -1,10 +1,12 @@
 import Pavilion from './Pavilion.vue';
+import Gallery from './Gallery.vue';
 
 export default [
   {
     path: '/pavilion',
     components: {
       default: Pavilion,
+      column: Gallery,
     },
     alias: ['/', '/pavello'],
   },
