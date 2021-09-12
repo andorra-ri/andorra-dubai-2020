310b002ce068c6c29e3b34fdd9f848a9a808774d
mvilella <mvilella@obsa.ad>
2021-09-12 19:07:40 +0400

    fix: Swap map image with map link

---
+++ src/layout/expo/Location.vue
@@ -0,0 +1,29 @@
+<template>
+  <a :href="address" target="blank" class="box">
+    <em>{{ t('location.district') }}</em>
+    <div class="at-bottom">
+      <h2>{{ t('location.find_us') }}<span class="arrow" /></h2>
+    </div>
+  </a>
+</template>
+
+<script>
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+export default {
+  name: 'Map',
+  setup() {
+    const { t, locale } = useI18n();
+    const address = computed(() => `https://www.expo2020dubai.com/${locale.value}/understanding-expo/participants/country-pavilions/andorra#js-map-container`);
+    return { t, address };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.map {
+  min-height: 200px;
+}
+</style>
+
--- src/layout/expo/routes.js
+++ src/layout/expo/routes.js
@@ -1,7 +1,7 @@
 import Expo from './Expo.vue';
 import Gallery from './Gallery.vue';
 import MoreInfo from './MoreInfo.vue';
-import Map from './Map.vue';
+import Location from './Location.vue';
 
 export default [{
   path: '/expo',
@@ -9,7 +9,7 @@ export default [{
     default: Expo,
     column: Gallery,
     top: MoreInfo,
-    bottom: Map,
+    bottom: Location,
   },
   alias: ['/'],
 }];
